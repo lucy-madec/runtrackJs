ed34ce9d3f2f13ba4a2d4b128974f25cf4854dd4
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-03-25 14:39:06 +0100

    Création d'un bouton en JavaScript pour afficher les jours de la semaine en console du web.

---
+++ jour01/job05/script.js
@@ -0,0 +1,6 @@
+function afficherJoursSemaines() {
+    var joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (var i = 0; i < joursSemaines.length; i++) {
+        console.log(joursSemaines[i]);
+    }
+}
