045920e5a194e36ac9cf0702110531b639d5d1de
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-03-28 09:22:20 +0100

    Correction du job02 sur l'arc-en-ciel pour que le jeu 'puzzle' fonctionne correctement.

--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -1,57 +1,44 @@
 $(document).ready(function() {
-    var images = [
-        'images/arc1.png',
-        'images/arc2.png',
-        'images/arc3.png',
-        'images/arc4.png',
-        'images/arc5.png',
-        'images/arc6.png'
-    ];
-
-    var correctOrder = [0, 1, 2, 3, 4, 5]; // Ordre correct des images
-    var currentOrder = [];
-
-    function shuffleImages() {
-        $('#dropZone').empty(); // Vide la zone de dépôt
-        currentOrder = images.sort(() => Math.random() - 0.5); // Mélange les images
-        currentOrder.forEach(function(image, index) {
-            $('#dropZone').append('<img src="' + image + '" class="rainbowImage" draggable="true" data-index="' + index + '">');
-        });
+    // Fonction pour mélanger les images
+    function shuffleImages() {  
+        var images = $('.rainbow-img');
+        for (var i = 0; i < images.length; i++) {
+            var randomIndex = Math.floor(Math.random() * images.length);
+            var temp = images[i].src;
+            images[i].src = images[randomIndex].src;
+            images[randomIndex].src = temp;
+        }
     }
 
+    // Fonction pour vérifier si les images sont dans le bon ordre
     function checkOrder() {
-        var isCorrect = true;
-        $('#dropZone img').each(function() {
-            if ($(this).data('index') !== currentOrder.indexOf($(this).attr('src'))) {
-                isCorrect = false;
-                return false; // Arrête l'itération
+        var ordered = true;
+        var images = $('.rainbow-img');
+        var arcs = ['arc1', 'arc2', 'arc3', 'arc4', 'arc5', 'arc6'];
+        images.each(function (index) {
+            if (!$(this).attr('src').includes(arcs[index])) {
+                ordered = false;
+                return false; // Sortir de la boucle each dès qu'une image est mal placée
             }
         });
-        return isCorrect;
+        return ordered;
     }
 
-    $('#shuffle').click(function() {
+    // Bouton pour mélanger les images
+    $('#shuffle-btn').click(function() {
         shuffleImages();
         $('#message').text('');
     });
 
-    $('#dropZone').on('dragstart', '.rainbowImage', function(event) {
-        event.originalEvent.dataTransfer.setData('text/plain', $(this).attr('src'));
-    });
-
-    $('#dropZone').on('dragover', function(event) {
-        event.preventDefault();
-    });
-
-    $('#dropZone').on('drop', function(event) {
-        event.preventDefault();
-        var data = event.originalEvent.dataTransfer.getData('text/plain');
-        var img = $('<img src="' + data + '" class="rainbowImage" draggable="true">');
-        $(this).append(img);
-        if (checkOrder()) {
-            $('#message').text('Vous avez gagné').css('color', 'green');
-        } else {
-            $('#message').text('Vous avez perdu').css('color', 'red');
+    // Contrôler l'ordre des images après un drag and drop
+    $('#sortable-container').sortable({
+        revert: true,
+        stop: function() {
+            if(checkOrder()) {
+                $('#message').text('Vous avez gagné').css('color', 'green');
+            } else {
+                $('#message').text('Vous avez perdu').css('color', 'red');
+            }
         }
     });
 });
